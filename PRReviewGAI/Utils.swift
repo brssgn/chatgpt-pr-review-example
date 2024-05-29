61459686c2dca5251ad51d961b9d35574df10951
Boris <brssgn@icloud.com>
2024-05-29 15:05:39 +0300

    Add Utils file

---
+++ PRReviewGAI/Utils.swift
@@ -0,0 +1,14 @@
+//
+//  Utils.swift
+//  PRReviewGAI
+//
+//  Created by Boris on 29.05.2024.
+//
+
+import Foundation
+
+extension String {
+    var integerValue: Int {
+        Int(self)!
+    }
+}
